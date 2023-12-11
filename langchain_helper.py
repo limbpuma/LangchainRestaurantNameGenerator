7ac1d3c763ca2819e3c4441530007e07cd981954
LimberDev <limbpuma_de@hotmail.com>
2023-12-11 18:37:51 +0100

    added info for deploy

--- langchain_helper.py
+++ langchain_helper.py
@@ -2,18 +2,21 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
-
+import streamlit as st
 import openai
 import os
-
-import streamlit as st
 
 # Configurar la clave API de OpenAI utilizando st.secrets
 openai_api_key = st.secrets["openai_api_key"]
 openai.api_key = openai_api_key
 
 
-llm = OpenAI(temperature=0.6)
+try:
+    llm = OpenAI(temperature=0.6)
+    # Resto de tu código...
+except Exception as e:
+    st.error(f"Error al inicializar OpenAI: {e}")
+
 
 
 def generate_restaurant_name_and_items(cuisine): 
